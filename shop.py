1b9e8d5ceee611ec8bedcd314f44839136f56925
theboisloth <58494561+theboisloth@users.noreply.github.com>
2019-12-06 16:15:38 -0600

    Update shop.py

--- shop.py
+++ shop.py
@@ -1,7 +1,32 @@
 import inventory as inv
+
+FIREBOMBS = None
+SMALL_HEALTH_POTS = None
+HEALTH_POTS = None
+IRONSKIN_POTS = None
+
+def configure_difficulty(difficultyLevel):
+    pass
+
 def displayShopIntro():
-    print("Welcome, traveller, to my humble shop!")
+    print("You walked into the shoppe, the owner greets you.")
+    print("\033[1;31mShop Owner: \n"
+    "Welcome, traveller, to my humble shop!"
+    "\033[0;0m")
     print("Please, browse my wares!")
 
-def updateInv(itemValue):
+    global FIREBOMBS, SMALL_HEALTH_POTS, HEALTH_POTS, IRONSKIN_POTS
+    print(f"[1]Firebomb(x{FIREBOMBS})\n"
+    "[2]Small Health Potion(x{SMALL_HEALTH_POTS})\n"
+    "[3]Health Potion(x{HEALTH_POTS})\n"
+    "[4]Ironskin Potion(x{IRONSKIN_POTS})\n"
+    "[5]Item Info")
+    shoppe(1)
+
+
+def shoppe(itemValue):
     pass
+
+
+
+displayShopIntro()
