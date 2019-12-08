fdbaedb1f39ac8675525758c61cb4bdc9532bd0c
theboisloth <58494561+theboisloth@users.noreply.github.com>
2019-12-08 13:34:35 -0600

    Create wolf.py

---
+++ wolf.py
@@ -0,0 +1,10 @@
+import random
+#Unused file thus far, being built currently.
+#Wolf has no crit chance
+
+#Damage ranges
+BITE_DAMAGE_RANGE = ((1, 10), (5, 15), (8, 20))
+SCRATCH_DAMAGE_RANGE = ((10, 30), (15, 35), )
+
+def configure_difficulty(difficultyLevel):
+    global pass
